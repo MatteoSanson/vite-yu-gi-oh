d97dacb651d22d5ffa864185fcf228072dbb8416
MatteoSanson <matteo.sanson@gmail.com>
2023-12-12 22:10:41 +0100

    script: font montserrat

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
-import { createApp } from 'vue'
-import './assets/scss/style.scss'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './assets/scss/style.scss';
+//font montserrat
+import "@fontsource/montserrat";
+
+import App from './App.vue';
 
 createApp(App).mount('#app')
