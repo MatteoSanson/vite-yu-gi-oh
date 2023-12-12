08ebc60292ef12d099908b2835dd712102e369a4
MatteoSanson <matteo.sanson@gmail.com>
2023-12-12 23:02:32 +0100

    feat: add axios

--- src/components/main/CardList.vue
+++ src/components/main/CardList.vue
@@ -1,11 +1,31 @@
 <script>
+import axios from 'axios';
+import SingleCard from './SingleCard.vue';
+
 export default {
     name: 'CardList',
+    components: {
+        SingleCard,
+    },
+    data() {
+        return {
+            cards: [],
+            apiURL: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0',
+        }
+    },
+    created() {
+        axios.get(this.apiURL).then((response) => {
+            this.cards = response.data.data;
+            console.log(response.data);
+        });
+    }
 }
 </script>
 
 <template>
-    <div>lista carte</div>
+    <div>
+        <SingleCard v-for="card in cards" />
+    </div>
 </template>
 
 <style></style>
---
+++ src/components/main/SingleCard.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+    name: 'SingleCard',
+}
+</script>
+
+<template>
+    <div>card singola</div>
+</template>
+
+<style></style>
