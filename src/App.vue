eb298251b0026a595fabd292a2993f23c6454e66
Matteo Sanson <128544980+MatteoSanson@users.noreply.github.com>
2023-12-12 16:45:39 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: 'DefaultTemplate',
+
+  data() {
+    return {
+      text: 'Template Vite VueJS',
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>{{ text }}</h1>
+</template>
+
+<style scoped lang="scss"></style>
